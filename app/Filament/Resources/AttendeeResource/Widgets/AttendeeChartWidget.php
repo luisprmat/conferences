35da1c71d50f20310518cf6b7e0546a63099cc26
Luis Parrado <luisprmat@gmail.com>
2024-09-04 21:39:45 -0500

    Sync chart data with Table filters

--- app/Filament/Resources/AttendeeResource/Widgets/AttendeeChartWidget.php
+++ app/Filament/Resources/AttendeeResource/Widgets/AttendeeChartWidget.php
@@ -2,14 +2,19 @@
 
 namespace App\Filament\Resources\AttendeeResource\Widgets;
 
-use App\Models\Attendee;
+use App\Filament\Resources\AttendeeResource\Pages\ListAttendees;
 use Filament\Widgets\ChartWidget;
+use Filament\Widgets\Concerns\InteractsWithPageTable;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
 
 class AttendeeChartWidget extends ChartWidget
 {
+    use InteractsWithPageTable;
+
     protected int|string|array $columnSpan = 'full';
+
+    protected static ?string $pollingInterval = null;
 
     protected static ?string $maxHeight = '200px';
 
@@ -29,26 +34,34 @@ class AttendeeChartWidget extends ChartWidget
         ];
     }
 
+    protected function getTablePage(): string
+    {
+        return ListAttendees::class;
+    }
+
     protected function getData(): array
     {
         $filter = $this->filter;
 
+        $query = $this->getPageTableQuery();
+        $query->getQuery()->orders = [];
+
         $data = match ($filter) {
-            'week' => Trend::model(Attendee::class)
+            'week' => Trend::query($query)
                 ->between(
                     start: now()->subWeek(),
                     end: now(),
                 )
                 ->perDay()
                 ->count(),
-            'month' => Trend::model(Attendee::class)
+            'month' => Trend::query($query)
                 ->between(
                     start: now()->subMonth(),
                     end: now(),
                 )
                 ->perDay()
                 ->count(),
-            '3months' => Trend::model(Attendee::class)
+            '3months' => Trend::query($query)
                 ->between(
                     start: now()->subMonths(3),
                     end: now(),
