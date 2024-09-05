c808874560d3f0e1fd4d3c9060c6dd8f4613ec96
Luis Parrado <luisprmat@gmail.com>
2024-09-04 21:50:22 -0500

    Update stats widget dynamically with table search

--- app/Filament/Resources/AttendeeResource/Widgets/AttendeesStatsWidget.php
+++ app/Filament/Resources/AttendeeResource/Widgets/AttendeesStatsWidget.php
@@ -2,26 +2,34 @@
 
 namespace App\Filament\Resources\AttendeeResource\Widgets;
 
-use App\Models\Attendee;
+use App\Filament\Resources\AttendeeResource\Pages\ListAttendees;
+use Filament\Widgets\Concerns\InteractsWithPageTable;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class AttendeesStatsWidget extends BaseWidget
 {
+    use InteractsWithPageTable;
+
     protected function getColumns(): int
     {
         return 2;
     }
 
+    protected function getTablePage(): string
+    {
+        return ListAttendees::class;
+    }
+
     protected function getStats(): array
     {
         return [
-            Stat::make(__('Attendees Count'), Attendee::count())
+            Stat::make(__('Attendees Count'), $this->getPageTableQuery()->count())
                 ->description(__('Total number of attendees'))
                 ->descriptionIcon('heroicon-o-user-group')
                 ->color('success')
                 ->chart([4, 3, 1, 2, 5]),
-            Stat::make(__('Total Revenue'), Attendee::sum('ticket_cost')),
+            Stat::make(__('Total Revenue'), $this->getPageTableQuery()->sum('ticket_cost')),
         ];
     }
 }
